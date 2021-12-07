f81edd6723affbc7cef05ae3a90b3919752f42ca
Jasper Bok <hello@jasperbok.nl>
2021-12-07 21:25:21 +0100

    Make a dumb install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+cp .tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/nvim
+cp nvim/init.vim ~/.config/nvim/init.vim
+
+mkdir -p ~/.config/nvim/autoload
+wget "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+mv plug.vim ~/.config/nvim/autoload/plug.vim
+
+echo 'Start nvim and run :PlugInstall'
+
+mkdir -p ~/.config/fish
+cp fish/config.fish ~/.config/fish/config.fish
+cp fish/functions/* ~/.config/fish/functions/
